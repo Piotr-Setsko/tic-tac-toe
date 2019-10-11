56c4f193ea518018d6d35b008aceeb64e102520b
Piotr-Setsko <lihadzey@gmail.com>
2019-10-11 16:42:06 +0300

    feat: add method getCurrentPlayerSymbol()

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,14 +1,25 @@
 class TicTacToe {
     constructor() {
-
+      this.field = [];
+      this.player = 'x';
+      this.field = array(this.field);
+      function array(arr) {
+        for (let i = 0; i< 3 ; i++) {
+          arr[i] = [];
+            for (let j=0;j<3;j++){
+            arr[i][j] = null;
+          }
+        }
+        return arr;
+      }
     }
 
     getCurrentPlayerSymbol() {
-
+      return this.player;
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+      this.player = this.getFieldValue(rowIndex, columnIndex);
     }
 
     isFinished() {
@@ -28,8 +39,24 @@ class TicTacToe {
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+      if (this.field[rowIndex][colIndex] == null) {
+        if (this.player == 'x') {
+          this.field[rowIndex][colIndex] = 'x';
+          this.player = 'o';
+        } else {
+          this.field[rowIndex][colIndex] = 'o';
+          this.player = 'x';
+        }
+      } else {
+        if (this.player == 'o') {
+          this.player = 'o';
+        } else {
+          this.player = 'x';
+        }
+      }
+      return this.player;
     }
 }
 
 module.exports = TicTacToe;
+
